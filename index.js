0bf1f3026941cf05b2fb5084202dd2d450227674
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2021-01-15 07:32:31 +1300

    Adds 6539 support

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ rm4DeviceTypes[parseInt(0x653a, 16)] = "Broadlink RM4 Mini";
 rm4DeviceTypes[parseInt(0x6070, 16)] = "Broadlink RM4 Mini C";
 rm4DeviceTypes[parseInt(0x62be, 16)] = "Broadlink RM4 Mini C";
 rm4DeviceTypes[parseInt(0x610f, 16)] = "Broadlink RM4 Mini C";
+rm4DeviceTypes[parseInt(0x6539, 16)] = "Broadlink RM4 Mini C";
 rm4DeviceTypes[parseInt(0x648d, 16)] = "Broadlink RM4 Mini S";
 rm4DeviceTypes[parseInt(0x2225, 16)] = 'Manual RM4 Device';
 
