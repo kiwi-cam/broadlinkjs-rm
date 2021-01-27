579e34867299045c970a1f6355bf3dc873829e4f
Cameron <32912464+kiwi-cam@users.noreply.github.com>
2021-01-27 13:59:10 +1300

    Update index.js

--- index.js
+++ index.js
@@ -288,7 +288,10 @@ class Device {
 
     // Dynamically add relevant RF methods if the device supports it
     const isRFSupported = rmPlusDeviceTypes[parseInt(deviceType, 16)] || rm4PlusDeviceTypes[parseInt(deviceType, 16)];
-    if (isRFSupported) this.addRFSupport();
+    if (isRFSupported) {
+      this.log(`\x1b[35m[INFO]\x1b[0m Adding RF Support to device ${macAddress} with type ${deviceType.toString(16)}`);
+      this.addRFSupport();
+    }
   }
 
   // Create a UDP socket to receive messages from the broadlink device.
